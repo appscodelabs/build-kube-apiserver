d97320b7bdf72c3db9d48ef93d81f0c8c6bc09f6
Tamal Saha <tamal@appscode.com>
2018-06-13 16:52:08 -0700

    added-all

--- make.sh
+++ make.sh
@@ -4,11 +4,12 @@ set -x
 dir=`pwd`
 
 pushd ~/go/src/k8s.io/kuberbetes
+git_tag=$(git describe --exact-match --abbrev=0 2>/dev/null || echo '')
 make kube-apiserver
 cp _output/bin/kube-apiserver $dir/kube-apiserver
 popd
 
 chmod +x kube-apiserver
-docker build -t appscode/kube-apiserver-amd64:v1.10.3 .
-docker push appscode/kube-apiserver-amd64:v1.10.3
+docker build -t appscode/kube-apiserver-amd64:$git_tag .
+docker push appscode/kube-apiserver-amd64:$git_tag
 rm -rf kube-apiserver
